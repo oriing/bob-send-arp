ce87e6ba431da519d651b6253f697c9d5027f0b5
Park Jeongsik <ori@khu.ac.kr>
2024-08-13 10:45:38 -0400

    [edit] edit some bugs

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ void sendArp(Mac smac, Mac dmac, Ip sip, Ip tip, int mode){ // mode: ArpHdr::Rep
 
 void getLocalMac(){
 	// https://stackoverflow.com/questions/1779715/how-to-get-mac-address-of-your-machine-using-a-c-program
-	string	target = string("/sys/class/net/") + string(device) + string("/address")
+    string	target = string("/sys/class/net/") + string(device) + string("/address");
 	char	lmac[] = "00:00:00:00:00:00";
 
 	FILE *fp = fopen(target.c_str(), "r");
@@ -68,8 +68,8 @@ void getLocalMac(){
 		exit(-1);
 	}
 
-	fscanf(fp, "%s", lmac);
-	localMac = Mac(lmac);
+    fscanf(fp, "%s", lmac);
+    localMac = Mac(lmac);
 
 	printf("local mac address: %s\n", lmac);
 	fclose(fp);
@@ -82,22 +82,26 @@ void getMacFromArp(Ip tip, Ip usingip){
 	sendArp(localMac, Mac("ff:ff:ff:ff:ff:ff"), usingip, tip, ArpHdr::Request);
 
 	char  	errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1, errbuf);
+    pcap_t* handle = pcap_open_live(device, BUFSIZ, 1, 1, errbuf);
 	if (handle == nullptr) {
 		fprintf(stderr, "couldn't open device %s(%s)\n", device, errbuf);
 		exit(-1);
 	}
 
-	EthArpPacket  	packet;
-	u_char* 		data;
+    EthArpPacket  packet;
+    const u_char* data;
+    pcap_pkthdr*  temp;
 
 	while(1){
-		int res = pcap_next_ex(handle, &packet, &data);
+        int res = pcap_next_ex(handle, &temp, &data);
 		if(res==0) continue;
 		if(res==-1 || res==-2){
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(handle));
 			break;
 		}
+
+        EthArpPacket* packetPtr = (EthArpPacket*)temp;
+        packet = *packetPtr;
 
 		if(packet.eth_.type_ != htons(EthHdr::Arp))	continue;
 		if(packet.arp_.sip_  != htonl(tip)) 		continue;
